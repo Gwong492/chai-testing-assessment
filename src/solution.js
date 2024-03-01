93f96ee5e612363862b9b5dd7619e4b985d8eadd
Garrett Wong <123900256+Gwong@users.noreply.github.com>
2024-03-01 15:10:29 -0500

    added assessment files

---
+++ src/solution.js
@@ -0,0 +1,14 @@
+/*
+  Write a function that returns a particular student when given a name. If no name matches, return `null`.
+*/
+
+function findStudentByName(students, name) {
+  let found = students.find((student) => student.name === name)
+  if (found) {
+    return found
+  } else {
+    return null
+  }
+}
+
+module.exports = findStudentByName;
---
+++ test/solution.test.js
@@ -0,0 +1,25 @@
+// Write your tests here!
+const findStudentByName = require("../src/solution.js");
+const expect = require("chai").expect;
+
+describe("findStudentByName", () => {
+  it("Should return the entire obeject for the student if the name matches"), () => {
+    const students = [
+      { name: "Leo Yeon-Joo", score: 8.9 },
+      { name: "Morgan Sutton", score: 7.4 },
+      { name: "Natalee Vargas", score: 9.2 },
+    ];
+    const expected = { name: "Leo Yeon-Joo", score: 8.9 };
+    const actual = findStudentByName(students, "Leo Yeon-Joo");
+    expect(actual).to.eql(expected);
+  }
+  it("Should return null if name cannot be found"), () => {
+   const students = [
+      { name: "Leo Yeon-Joo", score: 8.9 },
+      { name: "Morgan Sutton", score: 7.4 },
+      { name: "Natalee Vargas", score: 9.2 },
+    ];
+    const actual = findStudentByName(students, "Mari Cornwell");
+    expect(actual).to.be.null;
+  }
+})
